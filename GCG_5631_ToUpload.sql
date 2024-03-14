f5a98ed500a61c99d1e1f5bc7097f554697b1012
Service Web <sw@gssmail.com>
2024-03-14 13:53:06 -0500

    Commit changes in C:\Websites\Git\emarlow\5631\GCG_5631_ToUpload.sql

---
+++ GCG_5631_ToUpload.sql
@@ -0,0 +1,15 @@
+CREATE TABLE "GCG_5631_ToUpload" (
+ "FileHeader" INTEGER,
+ "Num" INTEGER,
+ "Type" VARCHAR(7),
+ "Priority" VARCHAR(10),
+ "Part" VARCHAR(20),
+ "OpenQty" INTEGER,
+ "OrderID" VARCHAR(15),
+ "ItemID" VARCHAR(15),
+ "Locn" CHAR(4),
+ "ReqDate" DATE,
+ "NumFromForecast" INTEGER,
+ "A_1" CHAR(1),
+ "KeyFigure" VARCHAR(50),
+ "UploadGridNum" INTEGER );
